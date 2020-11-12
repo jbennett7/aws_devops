abe1d07bcc5302d43c4275b0470dbaf177d9cf77
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2020-11-11 23:05:51 -0500

    adds test main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider aws {
+  provider = 'default'
+  region = var.region
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
