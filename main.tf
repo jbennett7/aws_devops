b256aed60c081f45282b48f6d02c66459fa69f11
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2020-11-11 23:17:52 -0500

    fixes caller_arn

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ output "account_id" {
 }
 
 output "caller_arn" {
-  value = data.aws_caller_identity.current.account_id
+  value = data.aws_caller_identity.current.arn
 }
 
 output "caller_user" {
